f5e690996ba3ffdd1dc16f78bdf0f6e03b4e1c42
David Recuenco <david.recuenco@adidas-group.com>
2020-08-20 14:52:47 +0200

    ADD device wrapper for large screens

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,56 @@
 <template>
-  <ion-app>
-    <ion-menu side="start" menu-id="nav" content-id="main">
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Menu</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content scroll-y="false">
-        <ion-list>
-          <ion-item @click="push('/bookmarks')" :color="getItemColor('/bookmarks')" detail>
-            <ion-icon name="bookmark" slot="start" size="small"></ion-icon>
-            Home
+  <device-wrapper>
+    <ion-app>
+      <ion-menu side="start" menu-id="nav" content-id="main">
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Menu</ion-title>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content scroll-y="false">
+          <ion-list>
+            <ion-item @click="push('/bookmarks')" :color="getItemColor('/bookmarks')" detail>
+              <ion-icon name="bookmark" slot="start" size="small"></ion-icon>
+              Home
+            </ion-item>
+            <ion-item @click="push('/archive')" :color="getItemColor('/archive')" detail>
+              <ion-icon name="archive" slot="start" size="small"></ion-icon>
+              Archive
+            </ion-item>
+            <ion-item @click="push('/preferences')" :color="getItemColor('/preferences')" detail>
+              <ion-icon name="settings" slot="start" size="small"></ion-icon>
+              Preferences
+            </ion-item>
+          </ion-list>
+          <ion-item lines="none" v-if="user">
+            <ion-chip @click="goToUserProfile">
+              <ion-avatar>
+                <img :src="user.avatar" alt="User image">
+              </ion-avatar>
+              <ion-label>{{user.username}}</ion-label>
+            </ion-chip>
+            <ion-button @click="logOut()" color="danger" shape="round" fill="clear" slot="end">
+              Log out
+            </ion-button>
           </ion-item>
-          <ion-item @click="push('/archive')" :color="getItemColor('/archive')" detail>
-            <ion-icon name="archive" slot="start" size="small"></ion-icon>
-            Archive
+          <ion-item lines="none">
+            <div class="ion-text-center" style="width: 100%; font-size: .75rem">
+              App version 1.0.0-fdai7jrea8f
+            </div>
           </ion-item>
-          <ion-item @click="push('/preferences')" :color="getItemColor('/preferences')" detail>
-            <ion-icon name="settings" slot="start" size="small"></ion-icon>
-            Preferences
-          </ion-item>
-        </ion-list>
-        <ion-item lines="none" v-if="user">
-          <ion-chip @click="goToUserProfile">
-            <ion-avatar>
-              <img :src="user.avatar" alt="User image">
-            </ion-avatar>
-            <ion-label>{{user.username}}</ion-label>
-          </ion-chip>
-          <ion-button @click="logOut()" color="danger" shape="round" fill="clear" slot="end">
-            Log out
-          </ion-button>
-        </ion-item>
-        <ion-item lines="none">
-          <div class="ion-text-center" style="width: 100%; font-size: .75rem">
-            App version 1.0.0-fdai7jrea8f
-          </div>
-        </ion-item>
-      </ion-content>
-    </ion-menu>
-    <ion-router-outlet id="main">
-      <keep-alive>
-        <ion-vue-router />
-      </keep-alive>
-    </ion-router-outlet>
-  </ion-app>
+        </ion-content>
+      </ion-menu>
+      <ion-router-outlet id="main">
+        <keep-alive>
+          <ion-vue-router />
+        </keep-alive>
+      </ion-router-outlet>
+    </ion-app>
+  </device-wrapper>
 </template>
 
 <script>
+import DeviceWrapper from './components/DeviceWrapper'
 import { getUser } from './services/user'
 
 export default {
@@ -76,6 +79,9 @@ export default {
   },
   async mounted() {
     this.user = await getUser()
+  },
+  components: {
+    DeviceWrapper
   }
 }
 </script>
---
+++ src/components/DeviceWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="device-wrapper">
+    <slot />
+  </div>
+</template>
+
+<style>
+  .device-wrapper {
+    display: contents;
+  }
+
+  @media only screen and (min-width: 1200px) {
+    body {
+      background: linear-gradient(#9dcbe8, #1fa0f2) !important;
+    }
+
+    .device-wrapper {
+      display: block;
+      place-self: center;
+      position: relative;
+      height: calc(100% - 100px);
+      width: 500px;
+      margin: 50px auto;
+      border: 5px solid #000000;
+      border-radius: 5px;
+      box-shadow: 2px 2px 10px 2px #000000;
+    }
+  }
+</style>
